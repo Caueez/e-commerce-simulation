95ac50c24dd83e18a69d566e2837c9e03f30b448
Caueez <cauedsm@gmail.com>
2026-02-13 23:06:57 -0300

    WIP: api gateway partial

--- services/api-gateway/src/api_gateway/container.py
+++ services/api-gateway/src/api_gateway/container.py
@@ -1,8 +1,19 @@
 
 
-
+from infra.cache.redis.redis_impl import RedisCache
+from infra.database.postgres.postgres import PostgresDatabase
+from infra.messaging.registry import MessageringRegistry
+from core.config import Settings
 
 
 class AppContainer:
-    def __init__(self):
-        self._messagering = None
+    def __init__(self) -> None:
+        self.settings = Settings()
+        self.msg_registry = MessageringRegistry()
+        self.redis = RedisCache("localhost", 6379)
+        self.repo = PostgresDatabase(host="localhost", port=5432, user="admin", password="admin", dbname="db")
+
+    async def bootstrap(self):
+        await self.msg_registry.build()
+        await self.redis.build()
+        await self.repo.connect()
--- services/api-gateway/src/api_gateway/lifespan.py
+++ services/api-gateway/src/api_gateway/lifespan.py
@@ -3,10 +3,16 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from api_gateway.container import AppContainer
 
+from infra.messaging.registry import MessageringRegistry
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    messaging_registry = MessageringRegistry()
+    container = AppContainer(messaging_registry)
+
+    app.state.container = container
     
     print("API Gateway is starting...")
     
